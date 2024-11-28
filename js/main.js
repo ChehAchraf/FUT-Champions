de67a84850bdc8f59920ac73055c0eab796ffebe
ChehAchraf <achrafchehboun@outlook.com>
2024-11-28 20:59:04 +0100

    add some other functions

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ let driblle_import = document.getElementById('dribbleImport');
 let defonce_import = document.getElementById('defenceImport');
 let physique_import = document.getElementById('physiqueImport');
 let rate_import = document.getElementById('rateimport');
-
-
+let allcards = [];
+let editbtn = [];
 const createTab = document.getElementById('createTab');
 const importTab = document.getElementById('importTab');
 const createPlayer = document.getElementById('createPlayer');
@@ -43,117 +43,188 @@ importTab.addEventListener('click', () => {
 get_from_json();
 
 createForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if(player_name.value.trim() == '' || position.value.trim() == '' || pac.value.trim() == '' || defonce.value.trim() == '' || shoot.value.trim() == '' || driblle.value.trim() == '' || physique.value.trim() == '') {
-        console.log('empty');
-    }
+  e.preventDefault();
+  if (player_name.value.trim() == '' || position.value.trim() == '' || pac.value.trim() == '' || defonce.value.trim() == '' || shoot.value.trim() == '' || driblle.value.trim() == '' || physique.value.trim() == '') {
+    console.log('Please fill in all fields');
+    return;
+  }
+  console.log('Player data is valid');
 });
 
 document.getElementById('importedName').addEventListener('change', function() {
-    fill_field();
-    document.getElementById('modal').style.display = 'flex'
+  fill_field();
+  document.getElementById('modal').style.display = 'flex';
 });
 
 document.getElementById('importForm').addEventListener('submit',function(e){
-    e.preventDefault();
-    fill_field();
-})
+  e.preventDefault();
+  fill_field();
+});
 
 function get_from_json() {
-    let conn = new XMLHttpRequest();
-    conn.open('GET', 'https://data.3lachstore.com/data.json', true);
-    conn.send();
-    conn.onreadystatechange = function() {
-        if(conn.readyState == 4 && conn.status == 200) {
-            data = JSON.parse(conn.responseText);
-            add_to_inport(data);
-        }
-    };
+  let conn = new XMLHttpRequest();
+  conn.open('GET', 'https://achraf.brofortech.com/pl.json', true);
+  conn.send();
+  conn.onreadystatechange = function() {
+    if (conn.readyState == 4 && conn.status == 200) {
+      data = JSON.parse(conn.responseText);
+      add_to_inport(data);
+    }
+  };
 }
 
 function add_to_inport(data) {
-    data.players.forEach(function(player) {
-        let option = document.createElement('option');
-        option.value = player.name;
-        option.textContent = player.name;
-        document.getElementById('importedName').appendChild(option);
-    });
+  data.players.forEach(function(player) {
+    let option = document.createElement('option');
+    option.value = player.name;
+    option.textContent = player.name;
+    document.getElementById('importedName').appendChild(option);
+  });
 }
 
 function fill_field() {
   let selected_player_name = document.getElementById('importedName').value;
   
   if (selected_player_name) {
-      const player_info = data.players.find(function(player) {
-          return player.name === selected_player_name;
+    const player_info = data.players.find(function(player) {
+      return player.name === selected_player_name;
+    });
+
+    if (player_info) {
+      if (isPlayerInList(player_info.name)) {
+        console.log('Player is already in the list');
+        return;
+      }
+
+      position_import.value = player_info.position;
+      shoot_import.value = player_info.shooting;
+      pass_import.value = player_info.passing;
+      driblle_import.value = player_info.dribbling;
+      defonce_import.value = player_info.defending;
+      physique_import.value = player_info.physical;
+      rate_import.value = player_info.rating;
+
+      let img = player_info.photo;
+      let name = player_info.name;
+
+      let temp_card = `
+        <div class="cards bg-[url('img/badge_gold.webp')] bg-center bg-no-repeat bg-cover w-1/2 h-[220px] flex flex-col justify-center items-center" data-id="${player_info.name}">
+          <div class="flex justify-start">
+            <div class="text-xs text-[#393218]">
+              <p class="font-extra-bold">${player_info.rating}</p>
+              <p class="font-semibold">${player_info.position}</p>
+            </div>
+            <div class="text-center text-xs text-[#393218] font-extra-bold">
+              <img src="${img}" alt="${name}" class="w-[70px]">
+              <p class="name text-[9px]">${name}</p>
+            </div>
+          </div>
+          <div class="boxes text-[9px] mt-4 text-[#393218] grid grid-cols-6 grid-rows-2 gap-[1px] justify-center items-center">
+            <div>PAC</div><div>SHO</div><div>PAS</div><div>DRI</div><div>DEF</div><div>PHY</div>
+            <div class="font-extra-bold">${player_info.pace}</div>
+            <div class="font-extra-bold">${player_info.shooting}</div>
+            <div class="font-extra-bold">${player_info.passing}</div>
+            <div class="font-extra-bold">${player_info.dribbling}</div>
+            <div class="font-extra-bold">${player_info.defending}</div>
+            <div class="font-extra-bold">${player_info.physical}</div>
+          </div>
+          <div class="flages grid grid-cols-2 gap-4 items-center">
+            <img src="https://cdn.sofifa.net/flags/ar.png" alt="" class="w-[10px]">
+            <img src="https://cdn.sofifa.net/meta/team/239235/120.png" alt="" class="w-[10px]">
+          </div>
+          <div id="edit" class="edit absolute hidden cursor-pointer">
+              <i class="fa-solid fa-pen text-white bg-red-500 p-2 rounded-full"></i>
+          </div>
+        </div>
+      `;
+
+      document.getElementById('modal').innerHTML = ''; 
+      document.getElementById('modal').insertAdjacentHTML('beforeend', temp_card);
+      
+      allcards = document.querySelectorAll('.cards');
+      editbtn = document.querySelectorAll('.edit');
+      
+      show_icon();
+      edit_modal(name);
+      
+      document.getElementById('btn_import').addEventListener('click', function() {
+        document.getElementById('cards-container').innerHTML += temp_card;
+        document.getElementById('modal').style.display = "none";
+        allcards = document.querySelectorAll('.cards');
+        editbtn = document.querySelectorAll('.edit');
+        show_icon();
+        edit_modal(name);
+      }, {once: true});
+    } else {
+      console.log('Player not found');
+    }
+  }
+}
+
+function isPlayerInList(playerName) {
+  return Array.from(allcards).some(card => card.getAttribute('data-id') === playerName);
+}
+
+function show_icon() {
+  allcards.forEach(card => {
+    let editIcon = card.querySelector('.edit');
+    
+    card.addEventListener('mouseenter', function(){
+      editIcon.style.display = "flex";
+    });
+
+    card.addEventListener('mouseleave', function(){
+      editIcon.style.display = "none";
+    });
+  });
+}
+
+function edit_modal(name) {
+  editbtn.forEach(btn => {
+    btn.addEventListener('click', function () {
+      document.getElementById('editplayerModal').style.display = "flex";
+      allcards.forEach(card => {
+        if (card.getAttribute('data-id') === name) {
+          document.getElementById("nameedit").value = name;
+        }
       });
+    });
+  });
+
+  allcards.forEach(card => {
+    card.addEventListener('click', function () {
+      const playerName = card.getAttribute('data-id');
+      const player_info = data.players.find(player => player.name === playerName);
 
       if (player_info) {
-          position_import.value = player_info.position;
-          shoot_import.value = player_info.shooting;
-          pass_import.value = player_info.passing;
-          driblle_import.value = player_info.dribbling;
-          defonce_import.value = player_info.defending;
-          physique_import.value = player_info.physical;
-          rate_import.value = player_info.rating;
-
-          let img = player_info.photo;
-          let name = player_info.name;
-
-          let temp_card = `
-            <div class="player-card bg-[url('img/badge_gold.webp')] w-1/5 h-[220px] flex flex-col justify-center items-center" data-name="${name}">
-              <div class="flex justify-start">
-                <div class="text-xs text-[#393218]">
-                  <p class="font-extra-bold">${player_info.rating}</p>
-                  <p class="font-semibold">${player_info.position}</p>
-                </div>
-                <div class="text-center text-xs text-[#393218] font-extra-bold">
-                  <img src="${img}" alt="${name}" class="w-[70px]">
-                  <p class="text-[9px]">${name}</p>
-                </div>
-              </div>
-              <div class="boxes text-[9px] mt-4 text-[#393218] grid grid-cols-6 grid-rows-2 gap-[1px] justify-center items-center">
-                <div>PAC</div><div>SHO</div><div>PAS</div><div>DRI</div><div>DEF</div><div>PHY</div>
-                <div class="font-extra-bold">${player_info.pace}</div>
-                <div class="font-extra-bold">${player_info.shooting}</div>
-                <div class="font-extra-bold">${player_info.passing}</div>
-                <div class="font-extra-bold">${player_info.dribbling}</div>
-                <div class="font-extra-bold">${player_info.defending}</div>
-                <div class="font-extra-bold">${player_info.physical}</div>
-              </div>
-              <div class="flages grid grid-cols-2 gap-4 items-center">
-                <img src="https://cdn.sofifa.net/flags/ar.png" alt="" class="w-[10px]">
-                <img src="https://cdn.sofifa.net/meta/team/239235/120.png" alt="" class="w-[10px]">
-              </div>
-            </div>`;
-          document.getElementById('modal').innerHTML = temp_card
-          document.getElementById('btn_import').addEventListener('click',function(e){
-            e.preventDefault();
-            if (isplayerinlist(name)) {
-              alert('This player is already in the list.');
-              return;
-          }
-          
-            document.getElementById('cards-container').innerHTML += temp_card;
-          })
-          document.getElementById('modal').style.display = "none";
-      } else {
-          console.log('Player not found');
+        const positionSelect = document.getElementById('positionedit');
+        positionSelect.innerHTML = '';
+        const option = document.createElement('option');
+        option.value = player_info.position;
+        option.textContent = player_info.position;
+        positionSelect.appendChild(option);
+        console.log('Player position selected: ', player_info.position);
       }
-  }
+    });
+  });
 }
+document.getElementById('addtofield').addEventListener('click',function(e){
+  e.preventDefault();
+  
+allcards.forEach((card) => {
 
+  const playerName = card.getAttribute('data-id');
+  
 
-function isplayerinlist(playerName) {
-  let existingPlayers = document.querySelectorAll('#cards-container .player-card');
-  for (let playerCard of existingPlayers) {
-      let cardName = playerCard.getAttribute('data-name');
-      if (cardName === playerName) {
-          return true; 
-      }
+  const playerInfo = data.players.find(player => player.name === playerName);
+  
+  if (playerInfo) {
+    
+    const playerPosition = playerInfo.position; 
+    
+
+  } else {
+    console.log(`Player ${playerName} not found in the data.`);
   }
-  return false;
-}
-
-
-
+});
+})
