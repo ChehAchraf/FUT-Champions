4a61f0848212de20ff63359609996ee3725272f3
ChehAchraf <achrafchehboun@outlook.com>
2024-11-29 21:02:29 +0100

    some improvement

--- js/main.js
+++ js/main.js
@@ -4,64 +4,86 @@ let createdPlayer = [];
 let playersdata = new XMLHttpRequest();
 playersdata.open("GET", "https://brofortech.com/players.json", true);
 playersdata.send();
-document.getElementById('create').addEventListener('click', function(e) {
-  e.preventDefault();
-    let name = document.getElementById('name');
-  let position = document.getElementById('position');
-  let pac = document.getElementById('pac');
-  let shoot = document.getElementById('shoot');
-  let pass = document.getElementById('pass');
-  let dribble = document.getElementById('dribble');
-  let defence = document.getElementById('defence');
-  let physique = document.getElementById('physique');
-    let nameRegex = /^[A-Za-z\s]+$/;
-    let statRegex = /^[1-9][0-9]?$|^100$/;  
-  let obj = {
-    id: createdPlayer.length,
-    name: name.value,
-    position: position.value,
-    pac: pac.value,
-    shoot: shoot.value,
-    pass: pass.value,
-    dribble: dribble.value,
-    defence: defence.value,
-    physique: physique.value
-  };
-  if (!name.value || !nameRegex.test(name.value)) {
-      document.getElementById('warningmodal').style.display = "flex"
-      document.getElementById('errormsg').innerText = "Add Only letters"
-    return;
-  }
-  if (!position.value) {
-    document.getElementById('warningmodal').style.display = "flex"
-    document.getElementById('errormsg').innerText = "Please select a player position"
-    return;
-  }
-  if (!statRegex.test(pac.value) || !statRegex.test(shoot.value) || !statRegex.test(pass.value) || 
-      !statRegex.test(dribble.value) || !statRegex.test(defence.value) || !statRegex.test(physique.value)) {
-    document.getElementById('warningmodal').style.display = "flex"
-    document.getElementById('errormsg').innerText = "Please enter valid stats (numbers between 1 and 100)."
-    return;
-  }
-  createdPlayer.push(obj);
-  console.log(createdPlayer);
-  });
+
 
 playersdata.onreadystatechange = function () {
   if (playersdata.readyState === 4 && playersdata.status === 200) {
     let ourdata = JSON.parse(playersdata.response);
     let data = ourdata.players;
-
+    document.getElementById("create").addEventListener("click", function (e) {
+      e.preventDefault();
+      let name = document.getElementById("name");
+      let position = document.getElementById("position");
+      let pac = document.getElementById("pac");
+      let shoot = document.getElementById("shoot");
+      let pass = document.getElementById("pass");
+      let dribble = document.getElementById("dribble");
+      let defence = document.getElementById("defence");
+      let physique = document.getElementById("physique");
+      let photo = document.getElementById('profile');
+      let flag = document.getElementById('flag');
+      let rate = document.getElementById('rate');
+      let nameRegex = /^[A-Za-z\s]+$/;
+      let statRegex = /^[1-9][0-9]?$|^100$/;
+      let obj = {
+        id: createdPlayer.length,
+        name: name.value,
+        position: position.value,
+        pace: pac.value,
+        shooting: shoot.value,
+        passing: pass.value,
+        dribbling: dribble.value,
+        defending: defence.value,
+        physical: physique.value,
+        photo: photo.value,
+        flag : flag.value,
+        logo : logo.value,
+        club : club.value,
+        rating : rate.value
+      };
+      if (!name.value || !nameRegex.test(name.value)) {
+        document.getElementById("warningmodal").style.display = "flex";
+        document.getElementById("errormsg").innerText = "Add Only letters";
+        return;
+      }
+      if (!position.value) {
+        document.getElementById("warningmodal").style.display = "flex";
+        document.getElementById("errormsg").innerText =
+          "Please select a player position";
+        return;
+      }
+      if (
+        !statRegex.test(pac.value) ||
+        !statRegex.test(shoot.value) ||
+        !statRegex.test(pass.value) ||
+        !statRegex.test(dribble.value) ||
+        !statRegex.test(defence.value) ||
+        !statRegex.test(physique.value)
+      ) {
+        document.getElementById("warningmodal").style.display = "flex";
+        document.getElementById("errormsg").innerText =
+          "Please enter valid stats (numbers between 1 and 100).";
+        return;
+      }
+      data.push(obj)
+    });
+ 
     function addFiltreplayerToPosition(positionFilter) {
-      const filteredPlayers = data.filter(player => player.position === positionFilter);
+      const bench = document.getElementById('bench');
+      const filteredPlayers = data.filter(
+        (player) => player.position === positionFilter
+      );
       const playerstoadd = document.getElementById("playerstoadd");
-      playerstoadd.innerHTML = '';
+      playerstoadd.innerHTML = "";
 
-      filteredPlayers.forEach(player => {
-        let div = document.createElement("div")
-        div.setAttribute("class", "cursor-pointer bg-goldcard bg-no-repeat bg-center bg-cover w-32 h-44 flex flex-col pt-8 items-center")
-        if(positionFilter !== "GK"){
-        div.innerHTML = `
+      filteredPlayers.forEach((player) => {
+        let div = document.createElement("div");
+        div.setAttribute(
+          "class",
+          "cursor-pointer bg-[url('img/badge_gold.webp')] bg-no-repeat bg-center bg-cover w-32 h-44 flex flex-col pt-8 items-center"
+        );
+        if (positionFilter !== "GK") {
+          div.innerHTML = `
           <div class="flex">
             <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
               <span class="mb-[-5px] font-bold">${player.rating}</span>
@@ -100,9 +122,9 @@ playersdata.onreadystatechange = function () {
             <img src="${player.flag}" alt="${player.name}">
             <img src="${player.logo}" alt="${player.club}">
           </div>
-        `
-    }else{
-        div.innerHTML = `
+        `;
+        } else {
+          div.innerHTML = `
           <div class="flex">
             <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
               <span class="mb-[-5px] font-bold">${player.rating}</span>
@@ -141,21 +163,18 @@ playersdata.onreadystatechange = function () {
             <img src="${player.flag}" alt="${player.name}">
             <img src="${player.logo}" alt="${player.club}">
           </div>
-        `
-    }
-    
-    div.addEventListener('click', function () {
-        let positionElement = document.querySelector(`[player-position="${player.position}"]`);
-        positionElement.classList.remove("bg-blackcard")
-        positionElement.classList.add("bg-goldcard")
-
-
-
+        `;
+        }
         
-        
+        div.addEventListener("click", function () {
+          let positionElement = document.querySelector(
+            `[player-position="${player.position}"]`
+          );
+          positionElement.classList.remove("bg-[url('img/bg.png')]");
+          positionElement.classList.add("bg-[url('img/badge_gold.webp')]");
 
-        if(positionFilter !== "GK"){
-          positionElement.innerHTML = `
+          if (positionFilter !== "GK") {
+            positionElement.innerHTML = `
             <div class="flex">
               <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
                 <span class="mb-[-5px] font-bold">${player.rating}</span>
@@ -195,7 +214,7 @@ playersdata.onreadystatechange = function () {
               <img src="${player.logo}" alt="${player.club}">
             </div>
           `;
-        }else{
+          } else {
             positionElement.innerHTML = `
           <div class="flex">
             <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
@@ -234,97 +253,99 @@ playersdata.onreadystatechange = function () {
           <div class="flex justify-center items-center w-3 gap-2">
             <img src="${player.flag}" alt="${player.name}">
             <img src="${player.logo}" alt="${player.club}">
-          </div>`
-        
-        }
+          </div>`;
+          }
 
-        
+          let img = document.createElement("img");
+          img.setAttribute(
+            "class",
+            "w-5 bg-red-500 rounded-full  hover:scale-110 relative bottom-[150px] left-[45px] hidden"
+          );
+          img.setAttribute("src", "/img/delete.svg");
+          img.setAttribute("alt", "deleteico");
 
-        let img = document.createElement("img")
-        img.setAttribute("class","w-5 bg-red-500 rounded-full  hover:scale-110 relative bottom-[150px] left-[45px] hidden")
-        img.setAttribute("src","/img/delete.svg");
-        img.setAttribute("alt","deleteico")
+          positionElement.addEventListener("mouseover", function () {
+            img.classList.remove("hidden");
+            img.classList.add("block");
+          });
+          positionElement.addEventListener("mouseleave", function () {
+            img.classList.remove("block");
+            img.classList.add("hidden");
+          });
 
-        
-        positionElement.addEventListener("mouseover",function(){
-            img.classList.remove("hidden")
-            img.classList.add("block")
-        })
-        positionElement.addEventListener("mouseleave",function(){
-            img.classList.remove("block")
-            img.classList.add("hidden")
-        })
-        
+          img.addEventListener("click", function (event) {
+            event.stopPropagation();
 
-        img.addEventListener("click",function(event){
+            const playerIndex = addedToSquadArr.findIndex(
+              (play) => play.name === player.name
+            );
 
-          event.stopPropagation();
-
-          const playerIndex = addedToSquadArr.findIndex(play => play.name === player.name);
-          
-          
-              addedToSquadArr.splice(playerIndex, 1);
-              document.getElementById("filtredplayermodal").classList.add("hidden");
-              document.getElementById("filtredplayermodal").classList.remove("flex");
-          
+            addedToSquadArr.splice(playerIndex, 1);
+            document
+              .getElementById("filtredplayermodal")
+              .classList.add("hidden");
+            document
+              .getElementById("filtredplayermodal")
+              .classList.remove("flex");
 
             positionElement.innerHTML = "";
-            let addImg = document.createElement('img');
-            
-            addImg.setAttribute("class","mt-9 w-8 cursor-pointer bg-black rounded-full hover:bg-red-500");
-            addImg.setAttribute("src","/img/add.svg")
+            let addImg = document.createElement("img");
+
+            addImg.setAttribute(
+              "class",
+              "mt-9 w-8 cursor-pointer bg-black rounded-full hover:bg-red-500"
+            );
+            addImg.setAttribute("src", "/img/add.svg");
 
             positionElement.appendChild(addImg);
 
-            positionElement.classList.add("bg-blackcard")
-            positionElement.classList.remove("bg-goldcard")
+            positionElement.classList.add("bg-[url('img/bg.png')]");
+            positionElement.classList.remove("bg-[url('img/badge_gold.webp')]");
 
-            document.getElementById("filtredplayermodal").classList.add("hidden");
-            document.getElementById("filtredplayermodal").classList.remove("flex");
-            
-        })
-        
-        positionElement.appendChild(img)
-        
-        // add players added to squad to an array
-        function updateSquadPlayers(){
-        addedToSquadArr.push(player)
-        console.log(addedToSquadArr)
-      }
-      //player not added (le reste)
-      function noChoosenPlayers(){
-        
-      }
+            document
+              .getElementById("filtredplayermodal")
+              .classList.add("hidden");
+            document
+              .getElementById("filtredplayermodal")
+              .classList.remove("flex");
+          });
 
-        document.getElementById("filtredplayermodal").classList.add("hidden");
-        document.getElementById("filtredplayermodal").classList.remove("flex");
+          positionElement.appendChild(img);
+
+          // add players added to squad to an array
+          function updateSquadPlayers() {
+            addedToSquadArr.push(player);
+            console.log(addedToSquadArr);
+          }
+          function noChoosenPlayers() {}
+
+          document.getElementById("filtredplayermodal").classList.add("hidden");
+          document
+            .getElementById("filtredplayermodal")
+            .classList.remove("flex");
+        });
+        playerstoadd.appendChild(div);
       });
-  
-      
-      playerstoadd.appendChild(div);
-    
-    });
-  }
+    }
 
-    
-    document.querySelectorAll('.position-button').forEach(button => {
-      button.addEventListener('click', function () {
-        const position = button.getAttribute('player-position');
+    document.querySelectorAll(".position-button").forEach((button) => {
+      button.addEventListener("click", function () {
+        const position = button.getAttribute("player-position");
         addFiltreplayerToPosition(position);
-        
-        document.getElementById("filtredplayermodal").classList.add("flex");
-        document.getElementById("filtredplayermodal").classList.remove("hidden");
 
-      })
-    })
-    document.querySelectorAll(".substitution").forEach(button => {
-      button.addEventListener("click",function(){
-        
-        
         document.getElementById("filtredplayermodal").classList.add("flex");
-        document.getElementById("filtredplayermodal").classList.remove("hidden");
-      })
-    })
-    
+        document
+          .getElementById("filtredplayermodal")
+          .classList.remove("hidden");
+      });
+    });
+    document.querySelectorAll(".substitution").forEach((button) => {
+      button.addEventListener("click", function () {
+        document.getElementById("filtredplayermodal").classList.add("flex");
+        document
+          .getElementById("filtredplayermodal")
+          .classList.remove("hidden");
+      });
+    });
   }
 };
