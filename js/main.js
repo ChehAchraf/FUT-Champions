3977031fa06898d98a55a2989080180240325fa8
ChehAchraf <achrafchehboun@outlook.com>
2024-11-30 11:45:16 +0100

    fixed some issues

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let addedToSquadArr = [];
 let notAddTosquad = [];
 let createdPlayer = [];
 let playersdata = new XMLHttpRequest();
-playersdata.open("GET", "https://brofortech.com/players.json", true);
+playersdata.open("GET", "https://achraf.brofortech.com/pl.json", true);
 playersdata.send();
 
 
@@ -67,7 +67,112 @@ playersdata.onreadystatechange = function () {
       }
       data.push(obj)
     });
- 
+    
+    data.forEach(player => {
+      let temp_div = document.createElement("div");
+      temp_div.setAttribute(
+        "class",
+        "cursor-pointer bg-[url('img/badge_gold.webp')] bg-no-repeat bg-center bg-cover w-32 h-44 flex flex-col pt-8 items-center"
+      );
+      if(player.poysition !== "GK"){
+        temp_div.innerHTML = `
+          <div class="flex">
+            <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+              <span class="mb-[-5px] font-bold">${player.rating}</span>
+              <span class="text-[10px] font-medium">${player.position}</span>
+            </div>
+            <img class="w-20" src="${player.photo}" alt="${player.name}">
+          </div>
+          <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+          <div class="text-[#362f16] gap-1 flex">
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">PAC</span>
+              <span class="font-bold text-[10px]">${player.pace}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">SHO</span>
+              <span class="font-bold text-[10px]">${player.shooting}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">PAS</span>
+              <span class="font-bold text-[10px]">${player.passing}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">DRI</span>
+              <span class="font-bold text-[10px]">${player.dribbling}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">DEF</span>
+              <span class="font-bold text-[10px]">${player.defending}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">PHY</span>
+              <span class="font-bold text-[10px]">${player.physical}</span>
+            </div>
+          </div>
+          <div class="flex justify-center items-center w-3 gap-2">
+            <img src="${player.flag}" alt="${player.name}">
+            <img src="${player.logo}" alt="${player.club}">
+          </div>
+        `;
+        bench.appendChild(temp_div)
+      }else{
+        temp_div.innerHTML = `
+          <div class="flex">
+            <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+              <span class="mb-[-5px] font-bold">${player.rating}</span>
+              <span class="text-[10px] font-medium">${player.position}</span>
+            </div>
+            <img class="w-20" src="${player.photo}" alt="${player.name}">
+          </div>
+          <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+          <div class="text-[#362f16] gap-1 flex">
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">DIV</span>
+              <span class="font-bold text-[10px]">${player.diving}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">HAN</span>
+              <span class="font-bold text-[10px]">${player.handling}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">KIC</span>
+              <span class="font-bold text-[10px]">${player.kicking}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">REF</span>
+              <span class="font-bold text-[10px]">${player.reflexes}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">SPE</span>
+              <span class="font-bold text-[10px]">${player.speed}</span>
+            </div>
+            <div class="flex flex-col gap-0 justify-center items-center">
+              <span class=" text-[7px] font-medium mb-[-4px]">POS</span>
+              <span class="font-bold text-[10px]">${player.positioning}</span>
+            </div>
+          </div>
+          <div class="flex justify-center items-center w-3 gap-2">
+            <img src="${player.flag}" alt="${player.name}">
+            <img src="${player.logo}" alt="${player.club}">
+          </div>
+        `;
+        bench.appendChild(temp_div)
+      }
+      let deleteIcon = document.createElement("img");
+      deleteIcon.setAttribute("src", "/img/delete.svg");
+      deleteIcon.setAttribute("alt", "Delete Icon");
+      deleteIcon.setAttribute("class", "w-5 bg-red-500 rounded-full  hover:scale-110 relative bottom-[140px] left-[45px] hidden");
+      temp_div.appendChild(deleteIcon);
+      temp_div.addEventListener("mouseover", () => {
+        deleteIcon.classList.remove("hidden");
+    });
+
+    temp_div.addEventListener("mouseleave", () => {
+        deleteIcon.classList.add("hidden");
+    }); 
+    })
+
     function addFiltreplayerToPosition(positionFilter) {
       const bench = document.getElementById('bench');
       const filteredPlayers = data.filter(
